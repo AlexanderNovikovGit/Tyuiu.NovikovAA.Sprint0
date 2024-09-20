89a32389a451bcba2a2fce5fe22e2cd618f472a6
Novikov Alexander <Tyuiu.NovikovAA1@outlook.com>
2024-09-20 22:29:51 +0500

    task7

--- Tyuiu.Novikov.Sprint0.Task7.V0.Lib/DataService.cs
+++ Tyuiu.Novikov.Sprint0.Task7.V0.Lib/DataService.cs
@@ -8,22 +8,16 @@ namespace Tyuiu.NovikovAA.Sprint0.Task7.V0.Lib
 {
     public class DataService
     {
-        public static int[] AdditionArrays(int[] nums1, int[] nums2)
+        public static object AdditionArrays(int[] nums1, int[] nums2)
         {
-            if (nums1.Length == nums2.Length)
-            {
-                int[] resultArray = new int[nums1.Length];
-                for (int i = 0; i < nums1.Length; i++)
+        
+            int[] resultArray = new int[nums1.Length];
+            for (int i = 0; i < nums1.Length; i++)
                 {
                     resultArray[i] = nums1[i] + nums2[i];
                 }
                 return resultArray;
-            }
-            else
-            {
-                Console.WriteLine("Массивы разной длинны");
-                return null;
-            }
+        
         }
     }
 }
